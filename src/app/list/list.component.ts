345c23e05b3bfbcb60de178d06eb2d9b88ea09e0
sbelbeck <belbeck.sam@gmail.com>
2021-12-08 00:46:42 -0500

    shopping list first app commit

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+	listItems:any[] = [];
+	newItem:string = '';
+	deleteItem:string = '';
+	duplicateMsg = '';
+  constructor() {
+		this.listItems = ["eggs", "milk", "cheese"];
+	 }
+
+  ngOnInit(): void {
+  }
+
+	addItem() {
+		this.duplicateMsg = '';
+		if(this.listItems.includes(this.newItem.toLowerCase())){
+			this.duplicateMsg =  'This item is already on your list.';
+		} else {
+			this.listItems.push(this.newItem.toLowerCase());
+			this.newItem = '';
+		}
+	}
+
+	removeItem(item:string){
+		this.deleteItem = item;
+		this.listItems.forEach((item,index)=>{
+			if(item==this.deleteItem) this.listItems.splice(index,1);
+	 });
+	}
+
+}
